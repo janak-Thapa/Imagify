b51bcea1a1abdd8aaaa9a3c8dc64a3dafa8538ad
janak-Thapa <ogjanak1@gmail.com>
2024-11-21 20:36:54 +0545

    add vercel web analytics

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,13 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import AppContextProvider from './context/AppContext.jsx'
-
+import { Analytics } from '@vercel/analytics/react';
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <AppContextProvider>
       
     <App/>
+    <Analytics />
   </AppContextProvider>
     
   </BrowserRouter>,
